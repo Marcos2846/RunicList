077d08547d8ad6e7d709fa45d411eb11a5391073
Marcos Isaias Garcia Martinez <xboxlive2846@gmail.com>
2026-03-08 13:31:57 -0600

    Initial Commit

---
+++ RunicList/RunicListApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunicListApp.swift
+//  RunicList
+//
+//  Created by Marcos Garcia on 08/03/26.
+//
+
+import SwiftUI
+
+@main
+struct RunicListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
